0fd2ba5c68636ded8d4f0cffc41211b5943c5afd
ptnga <pthuyngak3@gmail.com>
2024-06-03 16:13:16 +0700

    3

--- test1/firstApp/CustomCell.swift
+++ test1/firstApp/CustomCell.swift
@@ -4,18 +4,78 @@ import UIKit
 class MyTableViewCell: UITableViewCell {
     @IBOutlet private weak var textViewName: UILabel!
     @IBOutlet private weak var textViewContent: UILabel!
-    @IBOutlet private weak var cellImage: UIImageView!
     @IBOutlet private weak var textViewHour: UILabel!
     @IBOutlet private weak var labelNotify: UILabel!
+    @IBOutlet weak var position: UILabel!
+    @IBOutlet weak var imageName: UILabel!
+    @IBOutlet weak var lbDot: UILabel!
+    let colorBackgroundImage = UIColor(rgb: 0xFAD6DA, alpha: 1.0)
+    let colorBorder =  UIColor(rgb: 0x5DE64E, alpha: 1.0)
+    let colorFont = UIColor(rgb: 0xE65154, alpha: 1.0)
+    let colorView = UIColor(rgb: 0x303034, alpha: 1.0)
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        textViewName.translatesAutoresizingMaskIntoConstraints = false
+        textViewContent.translatesAutoresizingMaskIntoConstraints = false
+        textViewHour.translatesAutoresizingMaskIntoConstraints = false
+        // labelNotify.translatesAutoresizingMaskIntoConstraints = false
+        position.translatesAutoresizingMaskIntoConstraints = false
+        lbDot.translatesAutoresizingMaskIntoConstraints = false
+        setupConstraints()
+    }
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            textViewName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 97),
+            textViewName.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            lbDot.leadingAnchor.constraint(equalTo: textViewName.trailingAnchor, constant: 10),
+            lbDot.centerYAnchor.constraint(equalTo: textViewName.centerYAnchor),
+            lbDot.widthAnchor.constraint(equalToConstant: 6),
+            lbDot.heightAnchor.constraint(equalToConstant: 6),
+            position.leadingAnchor.constraint(equalTo: lbDot.trailingAnchor, constant: 10),
+            position.centerYAnchor.constraint(equalTo: textViewName.centerYAnchor),
+            position.trailingAnchor.constraint(lessThanOrEqualTo: contentView.trailingAnchor, constant: -16),
+            textViewContent.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 97),
+            textViewContent.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 45),
+            textViewContent.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -40),
+            textViewContent.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -30),
+//            labelNotify.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 351),
+//            labelNotify.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 42),
+            textViewHour.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 97),
+             textViewHour.topAnchor.constraint(equalTo: textViewContent.bottomAnchor, constant: 10),
+            textViewHour.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5),
+        ])
+    }
     public func set(user: ChatModel) {
-        cellImage.image = UIImage(named: user.nameImage)
+        imageName.text = user.nameImage
+        imageName.applyKerningLabel(1.3)
+        imageName.layer.cornerRadius = imageName.frame.height/2
+        imageName.layer.masksToBounds = true
+        imageName.layer.borderWidth = 1
+        imageName.layer.backgroundColor = colorBackgroundImage.cgColor
+        imageName.layer.borderColor = colorView.cgColor
         textViewName.text = user.username
+        textViewName.applyKerningLabel(1.3)
+        position.text = user.position
+        position.applyKerningLabel(1.3)
+        lbDot.layer.cornerRadius = lbDot.frame.height/2
+        lbDot.layer.masksToBounds = true
+        lbDot.layer.borderWidth = 1
+        lbDot.backgroundColor = UIColor.opaqueSeparator
+        lbDot.layer.borderColor = UIColor.opaqueSeparator.cgColor
+        lbDot.sizeToFit()
         textViewContent.text = user.title
-        textViewHour.text = user.timestamp
+        textViewContent.applyKerningLabel(1.3)
+        textViewHour.text = formatTimestamp(user.timestamp)
+        textViewName.sizeToFit()
+        position.sizeToFit()
         if( user.numberNotify>0 ) {
             textViewName.textColor = UIColor.white
+            position.textColor = UIColor.white
             textViewContent.font = UIFont.systemFont(ofSize: 16, weight: .bold)
             textViewContent.textColor = UIColor.white
+            lbDot.backgroundColor = UIColor.white
+            lbDot.layer.borderColor = UIColor.white.cgColor
             labelNotify.backgroundColor = .red
             labelNotify.text = "\(user.numberNotify)"
             labelNotify.layer.cornerRadius = labelNotify.frame.height/2
@@ -25,6 +85,15 @@ class MyTableViewCell: UITableViewCell {
         }
 
     }
+        private func formatTimestamp(_ timestamp: Int) -> String? {
+            let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "hh:mm a"
+            dateFormatter.amSymbol = "AM"
+            dateFormatter.pmSymbol = "PM"
+            return dateFormatter.string(from: date)
+        }
+
 }
 extension View4Controller {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -37,5 +106,20 @@ extension View4Controller {
         let user = users[indexPath.row]
         cell.set(user: user)
         return cell
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            users.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 300
     }
 }
--- test1/firstApp/View4Controller.swift
+++ test1/firstApp/View4Controller.swift
@@ -3,19 +3,41 @@ import UIKit
 public final class ChatModel {
     var nameImage: String
     var username: String
+    var position: String
     var title: String
-    var timestamp: String
+    var timestamp: Int
     var numberNotify: Int
-    init(nameImage: String, username: String, title: String, timestamp: String, numberNotify: Int) {
+    init(nameImage: String, username: String, position: String, title: String, timestamp: Int, numberNotify: Int) {
         self.nameImage = nameImage
         self.username = username
+        self.position = position
         self.title = title
         self.timestamp = timestamp
         self.numberNotify = numberNotify
     }
+    convenience init(json: [String: Any]) {
+        let username = json["username"] as? String ?? ""
+        let listNameFirst = username.split(separator: " ")
+        var firstInitial = ""
+        var secondInitial = ""
+
+        if let firstChar = listNameFirst[0].first {
+            firstInitial = String(firstChar)
+        }
+        if let secondChar = listNameFirst[1].first {
+            secondInitial = String(secondChar)
+        }
+        let nameImage = firstInitial + secondInitial
+        let position = json["position"] as? String ?? ""
+        let message = json["message"] as? String ?? ""
+        let timestamp = (json["time"] as? Int ?? 0)
+        let numberNotify = (json["numer_unread"] as? Int ?? 0)
+        
+        self.init(nameImage: nameImage, username: username, position: position, title: message, timestamp: timestamp, numberNotify: numberNotify)
+        }
 }
 class View4Controller: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    var users: [ChatModel] = []
+     var users: [ChatModel] = []
     let colorView = UIColor(rgb: 0x303034, alpha: 1.0)
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var addButton: UIButton!
@@ -53,30 +75,13 @@ class View4Controller: UIViewController, UITableViewDelegate, UITableViewDataSou
         self.navigationItem.rightBarButtonItem = rightButtonBar
         tableView.backgroundColor = colorView
         view.applyKerning(1)
-        let user1 = ChatModel(
-            nameImage: "user1",
-            username: "Erica Sanchez • Disatcher",
-            title: "I Called. Fell free to give m...",
-            timestamp: "02:45 PM",
-            numberNotify: 2
-        )
-        let user2 = ChatModel(
-            nameImage: "user2",
-            username: "Abraham Dawson • Disatcher",
-            title: "I Called. Fell free to give m...",
-            timestamp: "02:45 PM",
-            numberNotify: 0
-        )
-        let user3 = ChatModel(
-            nameImage: "user3",
-            username: "Sky High - All Drivers",
-            title: "I Called. Fell free to give m...",
-            timestamp: "02:45 PM",
-            numberNotify: 0
-        )
-        
-        users = [user1, user2, user3]
+        tableView.estimatedRowHeight = 300
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.beginUpdates()
+        tableView.endUpdates()
         tableView.reloadData()
+        readJSONFile()
+        
     }
     @objc private func didTapButton() {
         if let viewStartChat = UIStoryboard(
@@ -86,7 +91,25 @@ class View4Controller: UIViewController, UITableViewDelegate, UITableViewDataSou
             ) as? StartConversationController {
             navigationController?.pushViewController(viewStartChat, animated: true)
         }
-        //        let vc = startConversationController()
-        //        navigationController?.pushViewController(vc, animated: true)
     }
+    func readJSONFile() {
+            let fileName = "chat"
+            let fileType = "json"
+            if let path = Bundle.main.path( forResource: fileName, ofType: fileType ) {
+                do {
+                    let data = try Data(contentsOf: URL( fileURLWithPath: path ), options: .mappedIfSafe )
+                    if let jsonObject = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves) as? [String: Any],
+                       let jsonData = jsonObject["data"] as? [[String: Any]] {
+                        users.removeAll()
+                        for json in jsonData {
+                            let chatModel = ChatModel(json: json)
+                            users.append(chatModel)
+                        }
+                        tableView.reloadData()
+                    }
+                } catch {
+                    print( "Error reading JSON file: \( error ) " )
+                }
+            }
+        }
 }
